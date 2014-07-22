3b716e767d5e8c47eb9197e5e56ad64ff57f4c28
Stuart Young <stuartpyoung@gmail.com>
2014-07-22 12:30:38 -0700

    [0.8.0-beta.1+build.74] Updated docs for deploy.pl 'list' and 'all' --mode options

--- bin/install/deploy.pl
+++ bin/install/deploy.pl
@@ -36,29 +36,41 @@ sudo ./deploy.pl \
  [--printlog String] \
  [--help]
 
- --mode       :	deploy | bioapps | biorepo | ... options (see below)
- --configfile :	Location of configfile
- --logfile    :	Location of logfile
- --configfile :	Location of *.yaml config file
- --opsrepo    :	Name of ops repo (e.g., biorepodev, default: biorepo)
- --opsfile    :	Location of *.ops file containing configuration information
- --pmfile     :	Location of *.pm file containing installation instructions
- --repository :	Name of Git repository to be used as the code source
- --logfile    :	Location of log file
- --log    :	Print debug and other information to STDOUT (levels 1-5)
- --printlog   :	Print debug and other information to logfile (levels 1-5)
- --help       :	Print help info
+ --mode       :    deploy | bioapps | biorepo | ... options (see below)
+ --configfile :    Location of configfile
+ --logfile    :    Location of logfile
+ --configfile :    Location of *.yaml config file
+ --opsrepo    :    Name of ops repo (e.g., biorepodev, default: biorepo)
+ --opsfile    :    Location of *.ops file containing configuration information
+ --pmfile     :    Location of *.pm file containing installation instructions
+ --repository :    Name of Git repository to be used as the code source
+ --logfile    :    Location of log file
+ --log    	  :    Print debug and other information to STDOUT (levels 1-5)
+ --printlog   :    Print debug and other information to logfile (levels 1-5)
+ --help       :    Print help info
 
 The 'mode' options are as follows:
 
-package		Install a package (must specify with --package option)
-aguatest    Install the Agua tests package
-bioapps     Install the Bioapps package
-biorepo     Install the Biorepository package
-sge         Install the SGE (Sun Grid Engine) package
-starcluster Install the StarCluster package
-deploy      (DEFAULT) Do all of the above
+    --mode aguatest    Install the Agua tests package
+    --mode bioapps     Install the Bioapps package
+    --mode biorepo     Install the Biorepository package
+    --mode sge         Install the SGE (Sun Grid Engine) package
+    --mode starcluster Install the StarCluster package
+    --mode deploy      (DEFAULT) Do all of the above
 
+The --mode option can also be used to install other packages:
+
+    --mode install --package packagename
+                       Install the 'packagename' package
+
+Alternately, show the full list of installed packages and the latest versions of available packages:
+
+    --mode list
+    
+Or, install all of the listed packages:
+
+    --mode all
+    
 EXAMPLES
 
 Install all dependencies
@@ -78,8 +90,8 @@ $| = 1;
 
 my $whoami = `whoami`;
 if ( not $whoami =~/^root\s*$/ ) {
-	print "You must be root to run deploy.pl\n";
-	exit;
+    print "You must be root to run deploy.pl\n";
+    exit;
 }
 
 #### USE LIB
@@ -96,8 +108,8 @@ use Agua::DBaseFactory;
 use Conf::Yaml;
 
 #### GET OPTIONS
-my $mode         =	"deploy";
-my $configfile   =	"$Bin/../../conf/config.yaml";
+my $mode         =    "deploy";
+my $configfile   =    "$Bin/../../conf/config.yaml";
 my $opsrepo;
 my $opsfile;
 my $pmfile;
@@ -111,20 +123,20 @@ my $login;
 my $token;
 my $keyfile;
 my $password;
-my $logfile     =	"/tmp/agua-deploy.log";
-my $log      	=	2;
-my $printlog    =	5;
+my $logfile     =    "/tmp/agua-deploy.log";
+my $log          =    2;
+my $printlog    =    5;
 my $help;
 GetOptions (
     'mode=s'        => \$mode,
-    'methods=s'  	=> \$methods,
+    'methods=s'      => \$methods,
     'configfile=s'  => \$configfile,
-    'opsrepo=s'  	=> \$opsrepo,
-    'opsfile=s'  	=> \$opsfile,
-    'pmfile=s'  	=> \$pmfile,
+    'opsrepo=s'      => \$opsrepo,
+    'opsfile=s'      => \$opsfile,
+    'pmfile=s'      => \$pmfile,
     'versionfile=s' => \$versionfile,
-    'package=s'  	=> \$package,
-    'version=s'  	=> \$version,
+    'package=s'      => \$package,
+    'version=s'      => \$version,
     'repository=s'  => \$repository,
     'logfile=s'     => \$logfile,
     'log=i'     => \$log,
@@ -138,38 +150,38 @@ my $conf = Conf::Yaml->new(
     inputfile   =>  $configfile,
     backup      =>  1,
 
-    log			=>	$log,
-    printlog	=>	$printlog,
+    log            =>    $log,
+    printlog    =>    $printlog,
     logfile     =>  $logfile
 );
 
 
-$login 		= 	$ENV{'login'} if defined $ENV{'login'};
-$token 		= 	$ENV{'token'} if defined $ENV{'token'};
-$keyfile 	= 	$ENV{'keyfile'} if defined $ENV{'keyfile'};
-$password 	= 	$ENV{'password'} if defined $ENV{'password'};
+$login         =     $ENV{'login'} if defined $ENV{'login'};
+$token         =     $ENV{'token'} if defined $ENV{'token'};
+$keyfile     =     $ENV{'keyfile'} if defined $ENV{'keyfile'};
+$password     =     $ENV{'password'} if defined $ENV{'password'};
 #print "deploy.pl    login: $login\n";
 #print "deploy.pl    token: $token\n";
 
 my $object = Agua::Deploy->new({
     conf        =>  $conf,
     mode        =>  $mode,
-    methods  	=>  $methods,
+    methods      =>  $methods,
     configfile  =>  $configfile,
-    opsrepo  	=>  $opsrepo,
-    opsfile  	=>  $opsfile,
-    pmfile  	=>  $pmfile,
+    opsrepo      =>  $opsrepo,
+    opsfile      =>  $opsfile,
+    pmfile      =>  $pmfile,
     versionfile =>  $versionfile,
-    package  	=>  $package,
-    version  	=>  $version,
+    package      =>  $package,
+    version      =>  $version,
     repository  =>  $repository,
-    login  		=>  $login,
-    token  		=>  $token,
-    keyfile  	=>  $keyfile,
-    password  	=>  $password,
+    login          =>  $login,
+    token          =>  $token,
+    keyfile      =>  $keyfile,
+    password      =>  $password,
 
-    log			=>	$log,
-    printlog	=>	$printlog,
+    log            =>    $log,
+    printlog    =>    $printlog,
     logfile     =>  $logfile
 });
 
